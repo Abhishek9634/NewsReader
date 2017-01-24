c562a39d17ff6a65bbf70775f9d56926b888de42
Abhishek Thapliyal <abhishekthapliyal7579@gmail.com>
2017-01-24 21:40:05 +0530

    Manage VCs #3

---
+++ NewReader/NewReader/NRArticleCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NRArticleCollectionViewCell.swift
+//  NewReader
+//
+//  Created by Abhishek Thapliyal on 1/24/17.
+//  Copyright © 2017 Abhishek Thapliyal. All rights reserved.
+//
+
+import UIKit
+
+class NRArticleCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ NewReader/NewReader/NRSourceCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NRSourceCollectionViewCell.swift
+//  NewReader
+//
+//  Created by Abhishek Thapliyal on 1/24/17.
+//  Copyright © 2017 Abhishek Thapliyal. All rights reserved.
+//
+
+import UIKit
+
+class NRSourceCollectionViewCell: UICollectionViewCell {
+    
+}
